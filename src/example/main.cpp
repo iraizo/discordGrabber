52e64e975340c6350dd667e862f8f0034d6db41b
iraizo <admin@weeb.tech>
2020-06-06 19:35:37 +0200

    token impl

--- src/example/main.cpp
+++ src/example/main.cpp
@@ -6,12 +6,13 @@ int main(void)
 
 
     discordInformation info = manager.scan();
-    /*
+    
     std::cout << "environment: " << info.environment << std::endl;
     std::cout << "release: " << info.release << std::endl;
     std::cout << "username: " << info.user.username << std::endl;
     std::cout << "userid: " << info.user.id << std::endl;
     std::cout << "email: " << info.user.email << std::endl;
-    */
+    std::cout << "token: " << info.user.token << std::endl;
+    
     return 0;
 } 
